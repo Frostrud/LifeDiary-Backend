2e743e2dc3df5b280d2142f295216105d942bf9d
Frostrud <hofossfrostrud@gmail.com>
2023-03-17 00:33:55 +0100

    Inital commit.

---
+++ src/main/java/com/LifeDiarybackend/LifeDiaryBackendApplication.java
@@ -0,0 +1,13 @@
+package com.LifeDiarybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeDiaryBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeDiaryBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LifeDiarybackend/LifeDiaryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LifeDiarybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeDiaryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
