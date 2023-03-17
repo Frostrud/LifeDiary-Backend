98992cc7f5a1e20ee103d40264b04a6a5aba05d0
Frostrud <hofossfrostrud@gmail.com>
2023-03-17 02:35:09 +0100

    Started somewhat on the model logic for the backend.

---
+++ src/main/java/com/LifeDiarybackend/model/Item.java
@@ -0,0 +1,16 @@
+package com.LifeDiarybackend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    private Long itemID;
+    private String itemName;
+
+    public Item() {}
+}
